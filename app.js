25f39bc70b43343c6b7e897d6df4917f2afbfb79
Omer Tamir <omertamire@gmail.com>
2016-09-01 17:00:26 +0300

    added json file to delete and add objects

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 var bodyParser = require("body-parser");
+var fs = require('fs');
 
 app.use(bodyParser.json());  // add the app the option to parse data that send to the app on json type
 app.use(bodyParser.urlencoded({ extended: false })); // allways false (use only for real big amout of data)
@@ -10,18 +11,7 @@ app.use(express.static("./public"));  // make all the file on public static so t
 app.use(cors()); // allow other web and pages to read from my app
 
 var skierTerms = [
-    {
-        termx: "Rip",
-        defined: "To move at a high rate of speed"
-    },
-    {
-        termx: "Huck",
-        defined: "To throw your body off of something, usually a natural feature like a cliff"
-    },
-    {
-        termx: "Chowder",
-        defined: "Powder after it has been sufficiently skied"
-    }
+
 ];
 
 
@@ -30,12 +20,11 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-//// views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+
   response.render('public/index');
+    
 });
 
 app.use(function(req, res, next) {
@@ -44,18 +33,81 @@ app.use(function(req, res, next) {
 });
 
 app.get("/dictionary-api", function(req, res) { // router that open 
-	res.json(skierTerms); // json the skierTerms
+    
+    fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
+//         var jsonContent = JSON.parse(data3);
+        obj = JSON.parse(data);
+        obj = obj.MyActivitys;
+        obj.forEach(function(everyObject) { 
+        skierTerms.push({nameActivity: everyObject.nameActivity, Activity: everyObject.Activity}); 
+
+        });
+        	res.json(skierTerms); 
+        });
+
 });
+
+
 
 app.post("/dictionary-api", function(req, res) { // handle post for that page 
     skierTerms.push(req.body); // add the the array the req.push(data we got);
+
+    
+    fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
+//         var jsonContent = JSON.parse(data3);
+                obj = JSON.parse(data);
+        console.log("data "+obj)
+        append_new_Object(req.body);
+        });
+        
+        //obj.push(req.body);
+        
+        
+        
+    
+
     res.json(skierTerms);    // reload the data
 });
 
-app.delete("/dictionary-api/:termx", function(req, res) { // on delete 
-    skierTerms = skierTerms.filter(function(definition) { 
-        return definition.termx.toLowerCase() !== req.params.termx.toLowerCase(); // if the request is data term so delete
+app.delete("/dictionary-api/:nameActivity", function(req, res) { // on delete 
+    var valueToDel = req.params.nameActivity;
+    var posion = 0 ;
+    var flag = -1 ;
+
+    skierTerms = skierTerms.filter(function(definition) {
+        return definition.nameActivity.toLowerCase() !== req.params.nameActivity.toLowerCase(); // if the request is data term so delete
     });
+    
+    
+    fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
+        obj = JSON.parse(data); // data 
+        obj2 = obj.MyActivitys; // acitivitys
+            obj2.forEach(function(everyObject) {  // every object
+                flag++;
+                if(req.params.nameActivity.toLowerCase() == everyObject.nameActivity.toLowerCase())
+                  {
+                   posion = flag ;
+                   }
+    });
+        
+       
+    delete obj2[posion]
+obj2.splice(posion,1);
+
+
+
+
+    
+  var configJSON = JSON.stringify(obj);
+        
+                console.log(configJSON)
+
+  fs.writeFileSync('public/activity-data.json', configJSON);
+    
+    });
+   
+    
+
     res.json(skierTerms); // reload
 });
 
@@ -66,3 +118,28 @@ app.listen(app.get('port'), function() {
 });
 
 
+
+function delete_object(num){
+    
+   fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
+        obj = JSON.parse(data); // data 
+        obj = obj.MyActivitys; // acitivitys
+   });
+    
+    
+    obj.MyActivitys.delete[num];
+  obj.MyActivitys.delete(num);
+  var configJSON = JSON.stringify(obj);
+  fs.writeFileSync('public/activity-data.json', configJSON);
+    
+    
+    
+}
+
+function append_new_Object(obj){
+  var configFile = fs.readFileSync('public/activity-data.json');
+  var config = JSON.parse(configFile);
+  config.MyActivitys.push(obj);
+  var configJSON = JSON.stringify(config);
+  fs.writeFileSync('public/activity-data.json', configJSON);
+}
--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -1,21 +1,32 @@
 $(document).ready(function () { // on fuction ready ( someting JQuery need)
+      
+    
+    $( window ).load(function() {
+  //  $('body>dl').empty();//clean my dl tag
 
+    });
+    
+    
     $.getJSON('/dictionary-api', printTerms);//go to the adress '/dictionary-api' and take the paramtes and send the to the function printTerms
     
     
     $('form').submit(function (e) {
         e.preventDefault();
-        $.post('/dictionary-api', {termx: $('#term').val(), defined: $('#defined').val()}, printTerms);
+  
+        $.post('/dictionary-api', {nameActivity: $('#name_activity').val(), Activity: $('#activity_description').val()}, printTerms);
         this.reset();
+
+      
     });
 
 });
 
 function printTerms(terms) { // got the json file on terms .
+    
     $('body>dl').empty();//clean my dl tag
     $.each(terms, function () { // for each term in the terms json (for each json object)
-        $('<dt>').text(this.termx).appendTo('body>dl');  // create <dt> with the term value and add in to the dl
-        $('<dd>').text(this.defined).appendTo('body>dl');  //create <dd> with the defined value and add in to the dl
+        $('<dt>').text(this.nameActivity).appendTo('body>dl');  // create <dt> with the term value and add in to the dl
+        $('<dd>').text(this.Activity).appendTo('body>dl');  //create <dd> with the Activity value and add in to the dl
     });
     $('dt').off('dblclick').dblclick(function() { // on double click on 'dt'
         $.ajax({
