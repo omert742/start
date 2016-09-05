cd94e56cdd6992361cdc9e3f2a1f2e4210f4d5ca
Omer Tamir <omertamire@gmail.com>
2016-09-05 23:35:38 +0300

    new files and updates

--- app.js
+++ app.js
@@ -4,27 +4,26 @@ var app = express();
 var cors = require("cors");
 var bodyParser = require("body-parser");
 var fs = require('fs');
-
 app.use(bodyParser.json());  // add the app the option to parse data that send to the app on json type
 app.use(bodyParser.urlencoded({ extended: false })); // allways false (use only for real big amout of data)
 app.use(express.static("./public"));  // make all the file on public static so the app will know them
 app.use(cors()); // allow other web and pages to read from my app
-
 var skierTerms = [
+ 
+]; // my activitys
+var flagChecker = [2] ; // check if user exists
 
-];
 
 
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 app.get('/', function(request, response) {
-
-  response.render('public/index');
-    
+    response.render('public/index');
 });
 
 app.use(function(req, res, next) {
@@ -32,42 +31,31 @@ app.use(function(req, res, next) {
 	next();
 });
 
+app.post("/dictionary-api", function(req, res) { // router that open 
+    append_new_Object(req.body);
+});
+
 app.get("/dictionary-api", function(req, res) { // router that open 
-    
-    fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
-//         var jsonContent = JSON.parse(data3);
-        obj = JSON.parse(data);
-        obj = obj.MyActivitys;
-        obj.forEach(function(everyObject) { 
-        skierTerms.push({nameActivity: everyObject.nameActivity, Activity: everyObject.Activity}); 
+        console.log("On Get API");
+
+            fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
+            skierTerms = [];
+            obj = JSON.parse(data);
+            obj = obj.users[0].MyActivitys;
+            obj.forEach(function(everyObject) { 
+                skierTerms.push({nameActivity: everyObject.nameActivity, Activity: everyObject.Activity}); 
+            });
+                    res.json(skierTerms); 
 
         });
-        	res.json(skierTerms); 
-        });
+    
+
 
 });
 
 
 
-app.post("/dictionary-api", function(req, res) { // handle post for that page 
-    skierTerms.push(req.body); // add the the array the req.push(data we got);
 
-    
-    fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
-//         var jsonContent = JSON.parse(data3);
-                obj = JSON.parse(data);
-        console.log("data "+obj)
-        append_new_Object(req.body);
-        });
-        
-        //obj.push(req.body);
-        
-        
-        
-    
-
-    res.json(skierTerms);    // reload the data
-});
 
 app.delete("/dictionary-api/:nameActivity", function(req, res) { // on delete 
     var valueToDel = req.params.nameActivity;
@@ -78,10 +66,9 @@ app.delete("/dictionary-api/:nameActivity", function(req, res) { // on delete
         return definition.nameActivity.toLowerCase() !== req.params.nameActivity.toLowerCase(); // if the request is data term so delete
     });
     
-    
     fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
-        obj = JSON.parse(data); // data 
-        obj2 = obj.MyActivitys; // acitivitys
+        var obj = JSON.parse(data); // data 
+        var obj2 = obj.users[0].MyActivitys;
             obj2.forEach(function(everyObject) {  // every object
                 flag++;
                 if(req.params.nameActivity.toLowerCase() == everyObject.nameActivity.toLowerCase())
@@ -90,56 +77,123 @@ app.delete("/dictionary-api/:nameActivity", function(req, res) { // on delete
                    }
     });
         
-       
     delete obj2[posion]
-obj2.splice(posion,1);
-
-
-
-
-    
-  var configJSON = JSON.stringify(obj);
-        
-                console.log(configJSON)
-
-  fs.writeFileSync('public/activity-data.json', configJSON);
+    obj2.splice(posion,1);
+    var configJSON = JSON.stringify(obj);
+    fs.writeFileSync('public/activity-data.json', configJSON);
     
     });
-   
-    
-
     res.json(skierTerms); // reload
 });
 
 
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+
+
+
+
+
+app.get("/login", function(req, res) { // router that open 
+    
+        
+        console.log("On Login Get");
+    
+    res.json(flagChecker);
 });
+
+
+
+
+app.post("/login", function(req, res) { // handle post for that page 
+
+        console.log("On Login Post");
+        var Get_username = req.body.username.toLowerCase();
+            var Get_password = req.body.password.toLowerCase();
+    
+        fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
+            obj = JSON.parse(data);
+           obj = obj.users;
+            var flag = 0 ; //check if user exists
+          
+            
+                obj.forEach(function(everyObject) {  // every object
+                    if(Get_username.trim() === everyObject.username.trim() && Get_password === everyObject.password.trim() )
+                        {
+                                           console.log("Login");  
+                            flag = 1;
+                            flagChecker[0] = flag ;
+                        }
+        
+                });
+            
+            if(flag == 0)
+                {
+                    flagChecker[0] = flag ;
+                 //   alert("user is not exsists")
+                }
+
+        });
+
+
+});
+
+//TO DO registerr XXXXXXOOOOOXXXXOOOOOXXXXXOOOOXXXXXOOOOXXXXOOOOO
+app.post("/register", function(req, res) { // handle post for that page 
+
+    console.log("on post");
+        console.log("username : "+req.body.username );
+            console.log("password : "+req.body.password );
+
+});
+
+
+
+
+
+
 
 
 
 function delete_object(num){
     
    fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
-        obj = JSON.parse(data); // data 
-        obj = obj.MyActivitys; // acitivitys
+        var obj = JSON.parse(data); // data 
+        obj = obj.users[0].MyActivitys;
    });
     
-    
-    obj.MyActivitys.delete[num];
+  obj.MyActivitys.delete[num];
   obj.MyActivitys.delete(num);
   var configJSON = JSON.stringify(obj);
-  fs.writeFileSync('public/activity-data.json', configJSON);
-    
+   fs.writeFile('public/activity-data.json', configJSON, (err) => {
+          if (err){
+               throw err;
+          }
+        });
+        
     
     
 }
 
-function append_new_Object(obj){
-  var configFile = fs.readFileSync('public/activity-data.json');
-  var config = JSON.parse(configFile);
-  config.MyActivitys.push(obj);
-  var configJSON = JSON.stringify(config);
-  fs.writeFileSync('public/activity-data.json', configJSON);
+function append_new_Object(new_obj){
+    
+    fs.readFile('public/activity-data.json', 'utf8', function (err, data) {  
+        var obj = JSON.parse(data); // data 
+        var obj2 = obj.users[0].MyActivitys;
+        obj2.push(new_obj);
+        var configJSON2 = JSON.stringify(obj);
+        
+        
+        
+     fs.writeFile('public/activity-data.json', configJSON2, (err) => {
+          if (err){
+               throw err;
+          }
+        });
+        
+        });
+
+
 }
+
+
--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -10,23 +10,80 @@ $(document).ready(function () { // on fuction ready ( someting JQuery need)
     $.getJSON('/dictionary-api', printTerms);//go to the adress '/dictionary-api' and take the paramtes and send the to the function printTerms
     
     
-    $('form').submit(function (e) {
-        e.preventDefault();
-  
-        $.post('/dictionary-api', {nameActivity: $('#name_activity').val(), Activity: $('#activity_description').val()}, printTerms);
-        this.reset();
+    
+    $('#addText').submit(function (e) {
+         e.preventDefault();
+        $.post('/dictionary-api', {nameActivity: $('#name_activity').val(), Activity: $('#activity_description').val()});        this.reset();
 
-      
+         $.getJSON('/dictionary-api', printTerms);//go to the adress '/dictionary-api' and take the paramtes and send the to the function printTerms
+
+
+    });
+
+    
+    
+    
+//    
+//    
+//        $.getJSON('/login'), function(terms) { // router that open 
+//            console.log("On Login JS + e :"+terms);
+//            //this.reset();
+//                        //e.preventDefault();};
+//            
+            
+
+    
+    
+    $('#register').submit(function (e) {
+        e.preventDefault();
+        $.post('/login', {username: $('#username').val(),password: $('#password').val()}, printTerms);
+        this.reset();
+           
+        
+        
+        setTimeout(function (){
+
+  // Something you want delayed.
+
+}, 1000);
+        
+        
+                    
+    $.getJSON('/login', function(terms){
+          console.log("Fuck It flag " +terms[0]);
+        
+        if(terms[0] == 0) {
+                alert('Login Fail');
+            }
+        
+        else{
+            alert('Login Sueceessfully');
+        }
+
+    });//go to the adress '/dictionary-api' and take the paramtes and send the to the function printTerms
+    
     });
 
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
 function printTerms(terms) { // got the json file on terms .
     
-    $('body>dl').empty();//clean my dl tag
+    $('#mainDataBox').empty();//clean my dl tag
     $.each(terms, function () { // for each term in the terms json (for each json object)
-        $('<dt>').text(this.nameActivity).appendTo('body>dl');  // create <dt> with the term value and add in to the dl
-        $('<dd>').text(this.Activity).appendTo('body>dl');  //create <dd> with the Activity value and add in to the dl
+        $('<dt>').text(this.nameActivity).appendTo('#mainDataBox');  // create <dt> with the term value and add in to the dl
+        $('<dd>').text(this.Activity).appendTo('#mainDataBox');  //create <dd> with the Activity value and add in to the dl
     });
     $('dt').off('dblclick').dblclick(function() { // on double click on 'dt'
         $.ajax({
@@ -35,4 +92,5 @@ function printTerms(terms) { // got the json file on terms .
             success: printTerms // on success - reload the list 
         });
     });
+
 }
